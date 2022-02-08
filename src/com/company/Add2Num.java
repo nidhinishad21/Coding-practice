d98afab28630d57c0c1afc88ac987b547b7a4505
Nidhi <nidhinishad21@gmail.com>
2022-02-08 14:34:27 -0800

    First Project on Git

---
+++ src/com/company/Add2Num.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Add2Num {
+
+    public static int add2Numbers(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Add2Num.add2Numbers(2, 1);
+
+    }
+}
